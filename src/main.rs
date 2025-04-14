1c3e4593913ae146b44405765f38dde16bc452b7
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-04-14 22:13:45 +0200

    Implement guess number game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use rand;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is the guess number game!");
+    println!("Enter the number from 0 to 100");
+
+    let mut user_input: String = String::new();
+    let guessed_number: u32 = rand::random_range(0..=100);
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
+
+    let user_number: u32 = user_input.trim().parse().unwrap();
+
+    match user_number.cmp(&guessed_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("Bingo!!"),
+    }
 }
