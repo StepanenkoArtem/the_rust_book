7bb49434a712d609faf14421c82bb8b39e866dad
Artem Stepanenko <artem.stepanenko.ks.ua@gmail.com>
2025-04-26 10:10:45 +0200

    Add loop to tha game

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,23 @@ fn main() {
     println!("This is the guess number game!");
     println!("Enter the number from 0 to 100");
 
-    let mut user_input: String = String::new();
     let guessed_number: u32 = rand::random_range(0..=100);
 
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Failed to read line");
+    loop {
+        let mut user_input: String = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read line");
 
-    let user_number: u32 = user_input.trim().parse().unwrap();
+        let user_number: u32 = user_input.trim().parse().unwrap();
 
-    match user_number.cmp(&guessed_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Bingo!!"),
+        match user_number.cmp(&guessed_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Bingo!!");
+                break;
+            }
+        }
     }
 }
