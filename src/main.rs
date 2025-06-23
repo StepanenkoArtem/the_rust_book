d657dc4998cb034d57d6847d240fe74d68922aee
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-06-23 20:52:50 +0200

    rectungle area

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
-struct User {
-    name: String,
-    email: String,
-    age: i8,
+struct Figure {
+    width: u32,
+    height: u32,
 }
-
-struct Color(u8, u8, u8);
-struct AlwaysEqual;
 
 fn main() {
-    let mut new_user = build_user(12, String::from("Foobar"), String::from("ffobar@gmail.com"));
-    new_user.name = String::from("John Doe");
-    let another_user = User {
-        name: String::from("Foo Bar Bar Booo"),
-        ..new_user
+    let rectangle = Figure {
+        width: 114,
+        height: 34,
     };
+
     println!(
-        "{}, {} - {}",
-        another_user.name, another_user.age, another_user.email
-    );
-
-    let white = Color(255, 255, 255);
-    let black = Color(0, 0, 0);
-    example();
-    println!("Color code is {}, {}, {}", white.0, white.1, white.2);
-    println!("Color code is {}, {}, {}", black.0, black.1, black.2);
+        "The are of rectangel with width {} and height {} is {}",
+        rectangle.width,
+        rectangle.height,
+        area(&rectangle)
+    )
 }
 
-fn build_user(age: i8, name: String, email: String) -> User {
-    User { name, age, email }
-}
-
-fn example() -> AlwaysEqual { 
-    let example = AlwaysEqual;
-    example
+fn area(figure: &Figure) -> u32 {
+    figure.width * figure.height
 }
