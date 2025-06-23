a5a0933fc4df9ae7679862d2957b5839f3906f65
Artem Stepanenko <47758356+StepanenkoArtem@users.noreply.github.com>
2025-06-23 20:30:31 +0200

    Unit like struct

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ struct User {
 }
 
 struct Color(u8, u8, u8);
+struct AlwaysEqual;
 
 fn main() {
     let mut new_user = build_user(12, String::from("Foobar"), String::from("ffobar@gmail.com"));
@@ -20,10 +21,16 @@ fn main() {
 
     let white = Color(255, 255, 255);
     let black = Color(0, 0, 0);
+    example();
     println!("Color code is {}, {}, {}", white.0, white.1, white.2);
     println!("Color code is {}, {}, {}", black.0, black.1, black.2);
 }
 
 fn build_user(age: i8, name: String, email: String) -> User {
     User { name, age, email }
+}
+
+fn example() -> AlwaysEqual { 
+    let example = AlwaysEqual;
+    example
 }
